50b1e7b4bfd1e5bdf7975fc300f3a8f4c0988311
Rafikooo <thecrazyrafik@gmail.com>
2021-10-18 19:52:09 +0200

    foundry - init

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,10 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+         $product = new Product();
+         $product->setName("Shoes");
+         $product->setPrice(2137);
+         $manager->persist($product);
 
         $manager->flush();
     }
